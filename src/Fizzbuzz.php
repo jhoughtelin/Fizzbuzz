15f1088c88085e2001644c07d07949e7fa710f92
jhoughtelin <josh@findsomehelp.com>
2015-01-24 15:15:29 -0700

    removing else forever!

--- src/Fizzbuzz.php
+++ src/Fizzbuzz.php
@@ -92,12 +92,16 @@ class Fizzbuzz
     {
         if (($number % 3 == 0) && ($number % 5 == 0)) {
             return "FizzBuzz";
-        } elseif ($number % 3 == 0) {
-            return "Fizz";
-        } elseif ($number % 5 == 0) {
-            return "Buzz";
-        } else {
-            return $number;
         }
+
+        if ($number % 3 == 0) {
+            return "Fizz";
+        }
+
+        if ($number % 5 == 0) {
+            return "Buzz";
+        }
+
+        return $number;
     }
 }
